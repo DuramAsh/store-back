5b431639f2529b4c4c1bae27096bdea5a53eead5
DuramAsh <duramash.02@gmail.com>
2023-12-07 16:47:00 +0600

    add cors

--- pkg/server/router/http.go
+++ pkg/server/router/http.go
@@ -1,10 +1,9 @@
 package router
 
 import (
-	"time"
-
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/render"
 )
 
@@ -24,9 +23,15 @@ func New() *chi.Mux {
 
 	r.Use(middleware.Heartbeat("/"))
 
-	r.Use(middleware.Timeout(time.Second * 120))
-
 	r.Use(render.SetContentType(render.ContentTypeJSON))
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "PUT", "POST", "DELETE", "HEAD", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	return r
 }
